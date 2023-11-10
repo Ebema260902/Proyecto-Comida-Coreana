311a76446a426d69819ca60c3676d95a41938021
@Emanuel260902 <emanuel.aguero@ucr.ac.cr>
2023-11-10 08:44:02 -0600

    Cambios css en specifications.php

---
+++ specifications.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta name="viewport" content="width=device-width,user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Korean cuisine</title>
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@900&family=Roboto:wght@400;500&family=Inter:wght@400;800&display=swap" rel="stylesheet">
+    <!-- google fonts -->
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+
+<body>
+    <div class="main-container">
+
+        <!--HEADER -->
+        <header class="hero-container">
+            <nav class="top-nav">
+                <div>
+                    <a class="logo-image" href="#"><img src="./imgs/imgsproyect/Logo Kimchis 1imgs2.png" alt="Kimchis logo"></a>
+                </div>
+                    <a class="name-kimchis" href="./Homepage.html">KIMCHIS</a>
+                
+                <ul class="nav-list">
+                    <li><a class="nav-list-link" href="./Menu.html">Menu</a></li>
+                    <li><a class="nav-list-link" href="./Menu.html">Reservations</a></li>
+                </ul>
+
+                <ul class="nav-list-login-btn">
+                    <li><a class="btn-login nav-list-link" href="#">Login</a></li>
+                    <li><a class="btn-sign-up nav-list-link" href="#">Sign Up</a></li>
+                </ul>
+            </nav>
+            
+            <Section>
+                <h1 class="hero-title">Experience <br> Korean <br> Cuisine</h1>
+                <div>
+                    <p class="hero-text">Indulge in the vibrant and flavorful world of Korean gastronomy, where each dish is a masterpiece.</p>
+                </div>
+
+                <div class="btn-container">
+                    <a class="btn" href="#">GET STARTED</a>
+                </div>
+                <div>
+                    <img class="image-header" src="./imgs/imgsproyect/image-main33.jpg" alt="Image-Header">
+                </div>
+            </Section>
+        </header>
+        <!--HEADER -->
+
+
+      
+        <main>
+            <section class="container-dish-elements">
+                <h1>
+                    <img class="image-element" src="../Proyecto-Comida-Coreana/imgs/imgsproyect/Kimbapimgs.jpg" alt="Dish">
+                </h1>
+
+            </section>
+
+            <div class="bowl-container">
+                <img class="bowl-img" src="../Proyecto-Comida-Coreana/imgs/imgsproyect/Bowl Imágenimgs2.png" alt="Decorative Bowl">
+            </div>
+            </section>
+            <!--row 2-->
+
+            <!-- footer -->
+            <footer class="footer-container">
+                <div class="footer-content">
+                    <section>
+                        <h3 class="footer-title">Discover the Essence of<br>Korean Cuisine</h3>
+                        <p class"footer-text">At Kimchis, we have an array of delightful dishes that 
+                            capture <br> the essence of Korean cuisine. 
+                            Our mission is to share the <br> richness of Korean 
+                            flavors and traditions.</p>
+                    </section>
+                    <div class="footer-links">
+                        <!--Get to Know Us-->
+                        <section>
+                            <h3>Get to Know Us</h3>
+                            <ul class="nav-footer-list">
+                                <li> <a class="nav-footer-link" href="#">About Us</a> </li>
+                                <li> <a class="nav-footer-link" href="#">Policies</a> </li>
+                                <li> <a class="nav-footer-link" href="#">Accessibility</a> </li>
+                                <li> <a class="nav-footer-link" href="#">Address</a> </li>
+                                <li> <a class="nav-footer-link" href="#">Reservations</a> </li>
+                            </ul>
+                        </section>
+
+
+                        <!--Let Us Help You-->
+                        <section>
+                            <h3>Let Us Help You</h3>
+                            <ul class="nav-footer-list">
+                                <li> <a class="nav-footer-link" href="#">Your Account</a> </li>
+                                <li> <a class="nav-footer-link" href="#">Complains</a> </li>
+                                <li> <a class="nav-footer-link" href="#">Contact Us</a> </li>
+                                <li> <a class="nav-footer-link" href="#">Help Center</a> </li>
+                                <li> <a class="nav-footer-link" href="#">Submit Feedback</a> </li>
+                            </ul>
+                        </section>
+                    </div>
+                </div>
+                <section class="download-app">
+                    <h3>Get the App</h3>
+                    <div class="cta-app-container">
+                        <a href="#"><img src="./imgs/apple.png" alt="Our app from App Store"></a>
+                        <a href="#"><img src="./imgs/googleplay.png" alt="Our app from Google play"></a>
+                    </div>
+                </section>
+                <p class="footer-legal" >&copy; 2023. All rights reserved.</p>
+            </footer>
+            <!--footer-->
+        </main>
+    </div>
+</body>
+
+</html>
