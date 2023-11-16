2080ac2b532108fe370db7a3fdbc6d481a304a8f
@Emanuel260902 <emanuel.aguero@ucr.ac.cr>
2023-11-16 12:49:23 -0600

    Imlementación de un foreach para crear contenedores de manera dinamica mediante php en el homepage

--- Homepage.php
+++ Homepage.php
@@ -1,3 +1,28 @@
+
+<?php 
+    require_once '../database.php';
+    // Reference: https://medoo.in/api/select
+    // tb_dishes and tb_categories JOIN
+    $dishes = $database->select("tb_dish", [
+        "[>]tb_categories" => ["id_category" => "id_category"]
+    ], [
+        "tb_dish.id_dish",
+        "tb_dish.dish_name",
+        "tb_dish.dish_description",
+        "tb_dish.dish_image",
+        "tb_dish.dish_price",
+        "tb_dish.featured_dish",
+        "tb_categories.id_category",
+        "tb_categories.name_category" 
+    //], [
+        //where to show featured dishes only
+        //"tb_dishes.featured" => 1 
+    ]);
+
+    // var_dump($dishes);
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -45,7 +70,7 @@
                     <a class="btn-get-started-homepage" href="#">GET STARTED</a>
                 </div>
                 <div>
-                    <img class="image-header-homepage" src="./imgs/imgsproyect/image-main33.jpg" alt="Image-Header">
+                    <img class="image-header-homepage" src="./imgs/imgsproyect/image-main33.jpg" alt="Image-Header-Homepage">
                 </div>
             </Section>
         </header>
@@ -61,228 +86,36 @@
                 <div class="green-circle"></div>
                 
                 <!--row 1-->
-                <div class="dishes-container">
-                    <!--Kimchi-->
-                    <section class="dish">
-                        <h3 class="dish-title">Kimchi</h3>
-                        <div class="dish-thumb">
-                            <img class="dish-image" src="./imgs/imgsproyect/Kimchi 1imgs.jpg" alt="">
-                            <span class="dish-price">$6</span>
-                        </div>
-                    
-                        <p class="dish-text">Spicy and fermented cabbage <br> dish, seasoned with a blend of <br> red pepper flakes, garlic, ginger,<br>and other seasonings</p>
-                        <div>
-                            <a class="btn-cart" href="#"></a>
-                            <a class="btn-info" href="./Dish.html"></a>
-                        </div>
-                    </section>
                 
 
-                    <!--Bibimbap-->
-                    <section class="dish">
-                        <h3 class="dish-title">Bibimbap</h3>
-                        <div class="dish-thumb">
-                            <img class="dish-image" src="./imgs/imgsproyect/Bibimbapimgs.jpg" alt="">
-                            <span class="dish-price">$10</span>
-                        </div>
-                        <p class="dish-text">Vegetables, protein (beef or <br>tofu), fried egg, steamed rice.<br>Include gochujang sauce and garnished with sesame seeds.</p>
-                        <div class="featured-dishes-buttons">
-                            <a class="btn-cart" href="#"></a>
-                            <a class="btn-info" href="#"></a>
-                        </div>
-                    </section>
-
-                    <!--Korean barbecue-->
-                    <section class="dish">
-                        <h3 class="dish-title">Korean barbecue</h3>
-                        <div class="dish-thumb">
-                            <img class="dish-image" src="./imgs/imgsproyect/Krean Barbecueimgs.jpg" alt="">
-                            <span class="dish-price">$25</span>
-                        </div>
-                        <p class="dish-text">Marinated meat. Served with<br>sauces and accompanied by<br>lettuce leaves and other<br>condiments.</p>
-                        <div class="featured-dishes-buttons">
-                            <a class="btn-cart" href="#"></a>
-                            <a class="btn-info" href="#"></a>
-                        </div>
-                    </section>
-
-                    <!--Galbitang-->
-                    <section class="dish">
-                        <h3 class="dish-title">Galbitang</h3>
-                        <div class="dish-thumb">
-                            <img class="dish-image" src="./imgs/imgsproyect/Golbitangimgs.jpg" alt="">
-                            <span class="dish-price">$8</span>
-                        </div>
-                        <p class="dish-text">Beef ribs with spices, ginger,<br>and garlic. Served with rice<br>noodles, crisp vegetables, and<br>a touch of egg.</p>
-                        <div class="featured-dishes-buttons">
-                            <a class="btn-cart" href="#"></a>
-                            <a class="btn-info" href="#"></a>
-                        </div>
-                    </section>
-                </div>
-
-                <!--row 2-->
-                <div class="dishes-container">
-                    <!--Haemulpajeon-->
-                    <section class="dish">
-                        <h3 class="dish-title">Haemulpajeon</h3>
-                        <div class="dish-thumb">
-                            <img class="dish-image" src="./imgs/imgsproyect/Haemulpajeonimgs.jpg" alt="">
-                            <span class="dish-price">$6</span>
-                        </div>
-                        <p class="dish-text">Seafood and vegetable<br>pancake, crispy on the outside<br>and tender inside. Served with<br>dipping sauce.</p>
-                        <div class="featured-dishes-buttons">
-                            <a class="btn-cart" href="#"></a>
-                            <a class="btn-info" href="#"></a>
-                        </div>
-                    </section>
-
-                    <!--Bulgogi-->
-                    <section class="dish">
-                        <h3 class="dish-title">Bulgogi</h3>
-                        <div class="dish-thumb">
-                            <img class="dish-image" src="./imgs/imgsproyect/Bulgogiimgs.jpg" alt="">
-                            <span class="dish-price">$12</span>
-                        </div>
-                        <p class="dish-text">Marinated beef, grilled to<br>perfection. With hints of soy<br>sauce, garlic, and sugar in the<br>marinade.</p>
-                        <div class="featured-dishes-buttons">
-                            <a class="btn-cart" href="#"></a>
-                            <a class="btn-info" href="#"></a>
-                        </div>
-                    </section>
-
-                    <!--Tteokbokki-->
-                    <section class="dish">
-                        <h3 class="dish-title">Tteokbokki</h3>
-                        <div class="dish-thumb">
-                            <img class="dish-image" src="./imgs/imgsproyect/Tteokbokki.png" alt="">
-                            <span class="dish-price">$8</span>
-                        </div>
-                        <p class="dish-text">Chewy rice cakes cooked in<br>gochujang sauce,<br>accompanied by fish cakes<br>and scallions.</p>
-                        <div class="featured-dishes-buttons">
-                            <a class="btn-cart read-btn" href="#"></a>
-                            <a class="btn-info read-btn" href="#"></a>
-                        </div>
-                    </section>
-
-                    <!--Kimbap-->
-                    <section class="dish">
-                        <h3 class="dish-title">Kimbap</h3>
-                        <div class="dish-thumb">
-                            <img class="dish-image" src="./imgs/imgsproyect/Kimbapimgs.jpg" alt="">
-                            <span class="dish-price">$15</span>
-                        </div>
-                        <p class="dish-text">Rice roll wrapped and filled<br>with vegetables, pickled<br>radish, cooked egg and meat.</p>
-                        <div class="featured-dishes-buttons">
-                            <a class="btn-cart read-btn" href="#"></a>
-                            <a class="btn-info read-btn" href="#"></a>
-                        </div>
-                    </section>
-                </div>
-
-
-
-                    <!--row 3-->
-                <div class="dishes-container">
-
-                     <!--Bulgogi-->
-                     <section class="dish">
-                        <h3 class="dish-title">Bulgogi</h3>
-                        <div class="dish-thumb">
-                            <img class="dish-image" src="./imgs/imgsproyect/Bulgogiimgs.jpg" alt="">
-                            <span class="dish-price">$12</span>
-                        </div>
-                        <p class="dish-text">Marinated beef, grilled to<br>perfection. With hints of soy<br>sauce, garlic, and sugar in the<br>marinade.</p>
-                        <div class="featured-dishes-buttons">
-                            <a class="btn-cart" href="#"></a>
-                            <a class="btn-info" href="#"></a>
-                        </div>
-                    </section>
-
-                    <!--Tteokbokki-->
-                    <section class="dish">
-                        <h3 class="dish-title">Tteokbokki</h3>
-                        <div class="dish-thumb">
-                            <img class="dish-image" src="./imgs/imgsproyect/Tteokbokki.png" alt="">
-                            <span class="dish-price">$8</span>
-                        </div>
-                        <p class="dish-text">Chewy rice cakes cooked in<br>gochujang sauce,<br>accompanied by fish cakes<br>and scallions.</p>
-                        <div class="featured-dishes-buttons">
-                            <a class="btn-cart read-btn" href="#"></a>
-                            <a class="btn-info read-btn" href="#"></a>
-                        </div>
-                    </section>
-
-
-                    <!--Empty Container 1-->
-                    <section class="dish">
- 
-                    </section>
-
-                     <!--Empty Container 1-->
-                    <section class="dish">
-                    </section>
+                <?php 
+                echo "<div class='dishes-container-php'>";
+                foreach ($dishes as $dish) {
                     
-                </div>
+                        echo "<section class='dish'>";
+                            echo "<h3 class='dish-title-php'>".$dish["dish_name"]."</h3>";
+                            echo "<div class='dish-thumb'>";
+                                echo "<img class='dish-image' src='./imgs/".$dish["dish_image"]."'alt='".$dish["dish_name"]."'>";
+                            echo "</div>";
+                            echo "<p class='dish-text-php'>".substr($dish["dish_description"], 0, 90)."...</p>";
+                            echo "<div>";
+                                echo "<a class='btn-cart' href='#'></a>";
+                                echo "<a class='btn-info' href='./Dish.html'></a>";
+                            echo "</div>";
+                        echo "</section>";          
+                }
 
+                ?>
 
-            </div>
+                
 
-            <div class="bowl-container">
-                <img class="bowl-img" src="../Proyecto-Comida-Coreana/imgs/imgsproyect/Bowl Imágenimgs2.png" alt="Decorative Bowl">
-            </div>
-            </section>
-            <!--row 2-->
-
-            <!-- footer -->
-            <footer class="footer-container">
-                <div class="footer-content">
-                    <section>
-                        <h3 class="footer-title">Discover the Essence of<br>Korean Cuisine</h3>
-                        <p class"footer-text">At Kimchis, we have an array of delightful dishes that 
-                            capture <br> the essence of Korean cuisine. 
-                            Our mission is to share the <br> richness of Korean 
-                            flavors and traditions.</p>
-                    </section>
-                    <div class="footer-links">
-                        <!--Get to Know Us-->
-                        <section>
-                            <h3>Get to Know Us</h3>
-                            <ul class="nav-footer-list">
-                                <li> <a class="nav-footer-link" href="#">About Us</a> </li>
-                                <li> <a class="nav-footer-link" href="#">Policies</a> </li>
-                                <li> <a class="nav-footer-link" href="#">Accessibility</a> </li>
-                                <li> <a class="nav-footer-link" href="#">Address</a> </li>
-                                <li> <a class="nav-footer-link" href="#">Reservations</a> </li>
-                            </ul>
-                        </section>
-
-
-                        <!--Let Us Help You-->
-                        <section>
-                            <h3>Let Us Help You</h3>
-                            <ul class="nav-footer-list">
-                                <li> <a class="nav-footer-link" href="#">Your Account</a> </li>
-                                <li> <a class="nav-footer-link" href="#">Complains</a> </li>
-                                <li> <a class="nav-footer-link" href="#">Contact Us</a> </li>
-                                <li> <a class="nav-footer-link" href="#">Help Center</a> </li>
-                                <li> <a class="nav-footer-link" href="#">Submit Feedback</a> </li>
-                            </ul>
-                        </section>
-                    </div>
-                </div>
-                <section class="download-app">
-                    <h3>Get the App</h3>
-                    <div class="cta-app-container">
-                        <a href="#"><img src="./imgs/apple.png" alt="Our app from App Store"></a>
-                        <a href="#"><img src="./imgs/googleplay.png" alt="Our app from Google play"></a>
-                    </div>
-                </section>
-                <p class="footer-legal" >&copy; 2023. All rights reserved.</p>
-            </footer>
-            <!--footer-->
         </main>
-    </div>
+
+        <?php 
+            include "./parts/footer-homepage.php";
+        ?>
+        
+    
 </body>
 
 </html>
